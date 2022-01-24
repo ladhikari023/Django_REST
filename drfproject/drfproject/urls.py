0d41172e10beeab0a6df931a16ba8cb91228549e
Laxman Adhikari <ladhikari023@unm.edu>
2022-01-24 11:42:36 -0700

    Authentication for API use

--- drfproject/drfproject/urls.py
+++ drfproject/drfproject/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('',views.TestView.as_view(),name='test')
+    path('',views.TestView.as_view(),name='test'),
+    path('api/token/',obtain_auth_token,name='obtain')
 ]
